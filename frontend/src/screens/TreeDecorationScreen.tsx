ff25867f69384a7ac769c160c679b8023043b2ed
DanutSpafiu <spafiu.spafiu@yahoo.com>
2025-12-09 14:49:35 -0800

    added Tree Decoration Screen

---
+++ frontend/src/screens/TreeDecorationScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TreeDecorationScreen = () => {
+  return (
+    <View>
+      <Text>TreeDecorationScreen</Text>
+    </View>
+  )
+}
+
+export default TreeDecorationScreen
+
+const styles = StyleSheet.create({})
